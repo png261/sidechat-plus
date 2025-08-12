dda71bd7174318ada536e133f7db937686ffa3a6
png261 <nhphuong.code@gmail.com>
2025-08-12 17:06:29 +0700

    first commit

---
+++ lib/ai/providers.ts
@@ -0,0 +1,37 @@
+import {
+    customProvider,
+    extractReasoningMiddleware,
+    wrapLanguageModel,
+} from 'ai';
+import { google } from '@ai-sdk/google';
+import {
+    artifactModel,
+    chatModel,
+    reasoningModel,
+    titleModel,
+} from './models.test';
+import { isTestEnvironment } from '../constants';
+
+export const myProvider = isTestEnvironment
+    ? customProvider({
+        languageModels: {
+            'chat-model': chatModel,
+            'chat-model-reasoning': reasoningModel,
+            'title-model': titleModel,
+            'artifact-model': artifactModel,
+        },
+    })
+    : customProvider({
+        languageModels: {
+            'chat-model': google('gemini-2.0-flash-001'),
+            'chat-model-reasoning': wrapLanguageModel({
+                model: google('gemini-2.0-flash-001'),
+                middleware: extractReasoningMiddleware({ tagName: 'think' }),
+            }),
+            'title-model': google('gemini-2.0-flash-001'),
+            'artifact-model': google('gemini-2.0-flash-001'),
+        },
+        imageModels: {
+            'small-model': google.imageModel('gemini-2.0-flash-001'),
+        },
+    });
